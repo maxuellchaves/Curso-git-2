10c8fa49d524629779aa3f02906554890927c0f8
Maxuell Chaves <maxuellchaves30@gmail.com>
2020-04-10 00:07:14 -0300

    desejando um bom dia

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ namespace projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Olá mundo");
-            Console.WriteLine("Bom dia! ");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia ! ");
         }
     }
 }

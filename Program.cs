e3b8c9b612b5405b9fbcdcf116943da7dfe468b8
Maxuell Chaves <maxuellchaves30@gmail.com>
2020-04-10 12:49:49 -0300

    acrescentado traços

--- Program.cs
+++ Program.cs
@@ -10,9 +10,12 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("---------------------------");
             Console.WriteLine("Olá mundo");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia ! ");
+            Console.WriteLine("---------------------------");
+
         }
     }
 }
